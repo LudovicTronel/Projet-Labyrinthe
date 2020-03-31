0b794bffb530b11acd9ef95c43f7cd8d1f5e6e02
LudovicTronel <ludovic.tronel@etudiant.unc.nc>
2020-03-31 13:32:53 +0000

    Correction et optimisation fichier matrice.py + tests détaillés

--- matrice.py
+++ matrice.py
@@ -22,117 +22,119 @@ def Matrice(nbLignes, nbColonnes, valeurParDefaut=0):
 
 	return matrice 
 
-	matrice=Matrice(7,7)
-	for ligne in matrice:
-		print(ligne)
-
-
 def getNbLignes(matrice):
-    """
-    retourne le nombre de lignes de la matrice
-    paramètre: matrice la matrice considérée
-    """
-	return len(matrice)
+  """
+  retourne le nombre de lignes de la matrice
+  paramètre: matrice la matrice considérée
+  """
+  return len(matrice)
 	
 
 def getNbColonnes(matrice):
-    """
-    retourne le nombre de colonnes de la matrice
-    paramètre: matrice la matrice considérée
-    """
-	return len(matrice[0])
+  """
+  retourne le nombre de colonnes de la matrice
+  paramètre: matrice la matrice considérée
+  """
+  return len(matrice[0])
 
 
 def getVal(matrice,ligne,colonne):
-    """
-    retourne la valeur qui se trouve en (ligne,colonne) dans la matrice
-    paramètres: matrice la matrice considérée
-                ligne le numéro de la ligne (en commençant par 0)
-                colonne le numéro de la colonne (en commençant par 0)
-    """
-	return matrice[ligne][colonne]
+  """
+  retourne la valeur qui se trouve en (ligne,colonne) dans la matrice
+  paramètres: matrice la matrice considérée
+              ligne le numéro de la ligne (en commençant par 0)
+              colonne le numéro de la colonne (en commençant par 0)
+  """
+  return matrice[ligne][colonne]
 
 def setVal(matrice,ligne,colonne,valeur):
-    """
-    met la valeur dans la case se trouve en (ligne,colonne) de la matrice
-    paramètres: matrice la matrice considérée
-                ligne le numéro de la ligne (en commençant par 0)
-                colonne le numéro de la colonne (en commençant par 0)
-                valeur la valeur à stocker dans la matrice
-    cette fonction ne retourne rien mais modifie la matrice
-    """
-	matrice[ligne][colonne]=valeur
+  """
+  met la valeur dans la case se trouve en (ligne,colonne) de la matrice
+  paramètres: matrice la matrice considérée
+              ligne le numéro de la ligne (en commençant par 0)
+              colonne le numéro de la colonne (en commençant par 0)
+              valeur la valeur à stocker dans la matrice
+  cette fonction ne retourne rien mais modifie la matrice
+  """
+  matrice[ligne][colonne]=valeur
 
 #------------------------------------------        
 # decalages
 #------------------------------------------
 def decalageLigneAGauche(matrice, numLig, nouvelleValeur=0):
-    """
-    permet de décaler une ligne vers la gauche en insérant une nouvelle
-    valeur pour remplacer la premiere case à droite de cette ligne
-    le fonction retourne la valeur qui a été éjectée
-    paramèteres: matrice la matrice considérée
-                 numLig le numéro de la ligne à décaler
-                 nouvelleValeur la valeur à placer
-    résultat la valeur qui a été ejectée lors du décalage
-    """
-	matrice[numLig].append(nouvelleValeur)
-	valeur=matrice[numLig].pop(0)
-  	return valeur
+  """
+  permet de décaler une ligne vers la gauche en insérant une nouvelle
+  valeur pour remplacer la premiere case à droite de cette ligne
+  le fonction retourne la valeur qui a été éjectée
+  paramèteres: matrice la matrice considérée
+               numLig le numéro de la ligne à décaler
+               nouvelleValeur la valeur à placer
+  résultat la valeur qui a été ejectée lors du décalage
+  """
+  matrice[numLig].append(nouvelleValeur)
+  valeur=matrice[numLig].pop(0)
+  return valeur
 
 def decalageLigneADroite(matrice, numLig, nouvelleValeur=0):
-    """
-    decale la ligne numLig d'une case vers la droite en insérant une nouvelle
-    valeur pour remplacer la premiere case à gauche de cette ligne
-    paramèteres: matrice la matrice considérée
-                 numLig le numéro de la ligne à décaler
-                 nouvelleValeur la valeur à placer
-    résultat: la valeur de la case "ejectée" par le décalage
-    """
-	matrice[numLig].append(nouvelleValeur)
-	valeur=matrice[numLig].pop(-1)  #ne fonctionnne pas avec .append, car valeur enlevé puis rajouter ( a modifié )
-	return valeur 
+  """
+  decale la ligne numLig d'une case vers la droite en insérant une nouvelle
+  valeur pour remplacer la premiere case à gauche de cette ligne
+  paramèteres: matrice la matrice considérée
+               numLig le numéro de la ligne à décaler
+               nouvelleValeur la valeur à placer
+  résultat: la valeur de la case "ejectée" par le décalage
+  """
+  matrice[numLig].insert(0,nouvelleValeur)
+  valeur=matrice[numLig].pop(-1)
+  return valeur 
 
 def decalageColonneEnHaut(matrice, numCol, nouvelleValeur=0):
-    """
-    decale la colonne numCol d'une case vers le haut en insérant une nouvelle
-    valeur pour remplacer la premiere case en bas de cette ligne
-    paramèteres: matrice la matrice considérée
-                 numCol le numéro de la colonne à décaler
-                 nouvelleValeur la valeur à placer
-    résultat: la valeur de la case "ejectée" par le décalage
-    """
-	res=matrice[0][numCol]
-	for j in range(getNbLignes(matrice)-1):
-		matrice[j][numCol]=matrice[j+1][numCol]
-		matrice[-1][numCol]=nouvelleValeur
-	return res
+  """
+  decale la colonne numCol d'une case vers le haut en insérant une nouvelle
+  valeur pour remplacer la premiere case en bas de cette ligne
+  paramèteres: matrice la matrice considérée
+               numCol le numéro de la colonne à décaler
+               nouvelleValeur la valeur à placer
+  résultat: la valeur de la case "ejectée" par le décalage
+  """
+  valeur = matrice[0][numCol]
+  for i in range(len(matrice)-1):
+    matrice[i][numCol] = matrice[i+1][numCol]
+  matrice[len(matrice)-1][numCol] = nouvelleValeur
+  return valeur
 	
 
 def decalageColonneEnBas(matrice, numCol, nouvelleValeur=0):
-    """
-    decale la colonne numCol d'une case vers le bas en insérant une nouvelle
-    valeur pour remplacer la premiere case en haut de cette ligne
-    paramèteres: matrice la matrice considérée
-                 numCol le numéro de la colonne à décaler
-                 nouvelleValeur la valeur à placer
-    résultat: la valeur de la case "ejectée" par le décalage
-    """
-	res=matrice[-1][numCol]
-	for j in range(getNbLignes(matrice)-1):
-		matrice[j+1][numCol]=matrice[j][numCol]
-		matrice[0][numCol]=nouvelleValeur
-	return res
+  """
+  decale la colonne numCol d'une case vers le bas en insérant une nouvelle
+  valeur pour remplacer la premiere case en haut de cette ligne
+  paramèteres: matrice la matrice considérée
+               numCol le numéro de la colonne à décaler
+               nouvelleValeur la valeur à placer
+  résultat: la valeur de la case "ejectée" par le décalage
+  """
+  valeur = matrice[len(matrice)-1][numCol]
+  for i in range(len(matrice)-1,0,-1):
+    matrice[i][numCol] = matrice[i-1][numCol]
+  matrice[0][numCol] = nouvelleValeur
+  return valeur
 
 if __name__=='__main__':
-	m=Matrice(7,7)
-	print('verification création matrice:', m)
-	print('verif retourne nombre de ligne exact:', getNbLignes(m))
-	print('verif retourne nombre de colonne exact:', getNbColonnes(m))
-	print('verif récupérer la valeur en lignecolonne:', getVal(m,2,2))
-	print('verif mettre la valeur dans la case en ligne/colonne:', setVal(m,2,2,0))
-	print('verif décalage a gauche ligne:', decalageLigneAGauche(m,2,4))
-	print('verif décalage a droite ligne:', decalageLigneADroite(m,2,1))
-	print('verif décalage colonne en haut:', decalageColonneEnHaut(m,4,6))
-	print('verif décalage colonne en bas:', decalageColonneEnBas(m,5,1))
-
+  m=Matrice(7,7)
+  print('Vérification création matrice:    ', m)
+  print('Vérification nombre lignes:', getNbLignes(m))
+  print('Vérification nombre colonne:', getNbColonnes(m))
+  print('Vérification récupéreration valeur:', getVal(m,2,2))
+  setVal(m,2,0,1)
+  print('Initialisation décalage à gauche: ',m)
+  print('Vérification valeur éjectée:',decalageLigneAGauche(m,2,2))
+  print('Vérification décalage à gauche:   ', m)
+  print('Initialisation décalage à droite: ',m)
+  print('Vérification valeur éjectée:',decalageLigneADroite(m,2,3))
+  print('Vérification décalage à droite    ', m)
+  setVal(m,0,0,4)
+  print('Initialisation décalage en haut:  ',m)
+  print('Vérification valeur éjectée:', decalageColonneEnHaut(m,0,5))
+  print('Vérification décalage en haut:    ', m)
+  print('Vérification valeur éjectée:', decalageColonneEnBas(m,0,6))
+  print('Vérification décalage en bas:     ',m)
